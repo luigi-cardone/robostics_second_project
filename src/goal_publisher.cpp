850ecb8ab46e304456d9ceeebff2276f2317489e
Alexandra <alexandradumitrachee@gmail.com>
2024-06-23 18:31:46 +0200

    changed launch files

--- src/goal_publisher.cpp
+++ src/goal_publisher.cpp
@@ -1,9 +1,13 @@
 #include <ros/ros.h>
-#include <geometry_msgs/PoseStamped.h>
+#include <actionlib/client/simple_action_client.h>
+#include <move_base_msgs/MoveBaseAction.h>
 #include <fstream>
 #include <sstream>
 #include <string>
 #include <vector>
+#include <tuple>
+
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
 std::vector<std::tuple<double, double, double>> readGoalsFromCSV(const std::string &csv_file) {
     std::ifstream file(csv_file);
@@ -28,12 +32,26 @@ std::vector<std::tuple<double, double, double>> readGoalsFromCSV(const std::stri
     return goals;
 }
 
+void sendGoal(const MoveBaseClient& ac, const std::tuple<double, double, double>& goal) {
+    move_base_msgs::MoveBaseGoal goal_msg;
+    double x = std::get<0>(goal);
+    double y = std::get<1>(goal);
+    double theta = std::get<2>(goal);
+    goal_msg.target_pose.header.frame_id = "map";
+    goal_msg.target_pose.header.stamp = ros::Time::now();
+    goal_msg.target_pose.pose.position.x = x;
+    goal_msg.target_pose.pose.position.y = y;
+    goal_msg.target_pose.pose.orientation.z = sin(theta / 2.0);
+    goal_msg.target_pose.pose.orientation.w = cos(theta / 2.0);
+
+    ac.sendGoal(goal_msg);
+    ROS_INFO("Published waypoint at: %f %f %f", x, y, theta);
+}
+
 int main(int argc, char **argv) {
     ros::init(argc, argv, "goal_publisher");
     ros::NodeHandle nh("~");
 
-    ros::Publisher goal_pub = nh.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal", 1, true);
-    ros::Rate sleep_rate(0.05);
     std::string csv_file;
     if (!nh.getParam("csv_file", csv_file)) {
         ROS_ERROR("Failed to get param 'csv_file'");
@@ -41,29 +59,34 @@ int main(int argc, char **argv) {
     }
 
     std::vector<std::tuple<double, double, double>> goals = readGoalsFromCSV(csv_file);
-    ROS_INFO("%li have been load from %s", goals.size(), csv_file.c_str());
-    int current_goal = 0;
-    while (ros::ok() && current_goal < goals.size())
-    {
-        
-        geometry_msgs::PoseStamped goal_msg;
-        double x = std::get<0>(goals.at(current_goal));
-        double y = std::get<1>(goals.at(current_goal));
-        double z = std::get<2>(goals.at(current_goal));
-        goal_msg.header.frame_id = "map";
-        goal_msg.header.stamp = ros::Time::now();
-        goal_msg.pose.position.x = x;
-        goal_msg.pose.position.y = y;
-        goal_msg.pose.orientation.z = sin(z / 2.0);
-        goal_msg.pose.orientation.w = cos(z / 2.0);
+    ROS_INFO("%li goals have been loaded from %s", goals.size(), csv_file.c_str());
 
-        goal_pub.publish(goal_msg);
-        ROS_INFO("Published waypoint at: %f %f %f", x, y, z);
-        sleep_rate.sleep();
-        ros::spinOnce();
+    MoveBaseClient ac("move_base", true);
+    ROS_INFO("Waiting for the move_base action server...");
+    ac.waitForServer();
+    ROS_INFO("Connected to move_base server");
+
+    int current_goal = 0;
+    while (ros::ok() && current_goal < goals.size()) {
+        sendGoal(ac, goals[current_goal]);
+
+        bool goal_reached = false;
+        while (!goal_reached && ros::ok()) {
+            actionlib::SimpleClientGoalState state = ac.getState();
+            if (state == actionlib::SimpleClientGoalState::SUCCEEDED) {
+                ROS_INFO("Goal %d reached", current_goal);
+                goal_reached = true;
+            } else if (state == actionlib::SimpleClientGoalState::ABORTED) {
+                ROS_WARN("Goal %d aborted, retrying...", current_goal);
+                goal_reached = true; // Proceed to the next goal even if this one is aborted
+            }
+            ros::Duration(0.5).sleep();
+            ros::spinOnce();
+        }
+
         current_goal++;
     }
-    ROS_INFO("All goals have been published.");
 
+    ROS_INFO("All goals have been processed.");
     return 0;
 }
